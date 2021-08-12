13841596e66f8a06d154762b9a61f13daf0e6385
Bruno Zamorano Alves Monteiro WINDOWS <bzam1204@gmail.com>
2021-08-11 22:36:11 -0400

    card

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+var cartoes = []
+function criaCard() {
+    const conteudo_principal = document.querySelector('[data-main]')
+    const card = document.createElement('div')
+    const img = document.createElement('img')
+    const assunto = document.createElement('p')
+    const title = document.createElement('h2')
+    const sinopse = document.createElement('p')
+    const imgAutor = document.createElement('img')
+    const nomeAutor = document.createElement('p')
+    const data = document.createElement('p')
+    console.log(conteudo_principal)
+}
+criaCard()
