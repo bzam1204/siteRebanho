94c4175ad423abb737eb09af3bb1322b2e89bfc6
Bruno Zamorano Alves Monteiro WINDOWS <bzam1204@gmail.com>
2021-08-13 17:20:10 -0400

    autocreate cards

--- js/buscaCartoes.js
+++ js/buscaCartoes.js
@@ -1,26 +1,39 @@
 
-    var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://bzam1204.github.io/siteRebanho/cartoes.json");
+xhr.open("GET", "https://bzam1204.github.io/siteRebanho/cartoes.json");
 
-    xhr.addEventListener("load", function() {     
+xhr.addEventListener("load", function () {
 
-        if (xhr.status == 200) {
-            var resposta = xhr.responseText;
-            var cartoes = JSON.parse(resposta);
+    if (xhr.status == 200) {
+        var resposta = xhr.responseText;
+        var cartoes = JSON.parse(resposta);
 
-            cartoes.forEach(function (cartoes){
-                const main = document.querySelector('main')
-                const img = document.createElement('img')
-                main.appendChild(img)
-                img.src = cartoes.img
-                console.log(img.src)
-            } )
+        cartoes.forEach(function (cartoes) {
+            const main = document.querySelector('main')
+            const div = document.createElement('div')
+            div.classList.add('card')
+            main.appendChild(div)
+            div.innerHTML =
+                `
+                    <img class="imagemCard" src="${cartoes.img}" alt="">
+                    <p class="assunto">${cartoes.assunto}</p>
+                    <h2 class="titulo">${cartoes.title}</h2>
+                    <p class="sinopse">${cartoes.sinopse}</p>
+                    <a href="O Valor de Uma Alma.html" class="link">Leia Mais</a>
+                    <div class="molduraAutor">
+                        <img src="${cartoes.imgAutor}" alt="" class="imgAutor">
+                        <p class="nomeAutor">${cartoes.nomeAutor}</p>
+                        <p class="data">${cartoes.data}</p>
+                    </div>
+                `
+            console.log(cartoes.sinopse)
+        })
 
-        } else {
-            console.log('erro')
-        }
-    });
+    } else {
+        console.log('erro')
+    }
+});
 
-    xhr.send();
+xhr.send();
 
